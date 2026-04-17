e14f15127f18af359e9875527662debaf63fd46a
vlad <vf54640@gmail.com>
2026-04-17 21:46:50 +0300

    New progress

--- src/app/core/main.py
+++ src/app/core/main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from secure-access-vault!")
+import json
+import random
+from pathlib import Path
+
+ENCODING = 'utf-8'
+
+CONFIG_FOLDER_PATH = Path('src/app/configs')
+CONFIG_FILE_NAME = Path('user_db.json')
+CONFIG_ALL_PATH = Path(CONFIG_FOLDER_PATH) / CONFIG_FILE_NAME
+
+KEYS_FOLDER_PATH = Path('src/app/keys')
 
 
-if __name__ == "__main__":
-    main()
+def generate_token(user_name: str, access_level: int) -> str:
+    token = access_level * random.randint(1, 10)
+    log_string = f'TOKEN-{user_name.upper()}-{token}'
+    return log_string
+
+
+def save_token (log_string: str) -> str:
+    token = log_string.split('-')[2]
+    user_name = log_string.split('-')[0]
+    with open(Path(KEYS_FOLDER_PATH) / Path(f'{user_name}.txt'), 'w', encoding=ENCODING) as f:
+        f.write()
+
+
+with open(CONFIG_ALL_PATH, 'r', encoding=ENCODING) as f:
+    users_data = json.load(f)
+
+print(users_data)
--- src/app/create_input_data/create_user_db.py
+++ src/app/create_input_data/create_user_db.py
@@ -3,22 +3,31 @@ import json
 from typing import Any
 from pathlib import Path
 
-USERS = ['vlad', 'Sally', 'BOB', 'qwerty', 'Jon', '1234', 'Zeus', 'Apocriton', 'Arch']
+ENCODING = 'utf-8'
+FOLDER_PATH = Path('src/app/configs')
+FILE_NAME = Path('user_db.json')
+ALL_PATH = Path(FOLDER_PATH) / FILE_NAME
 
-def create_user_info(name: str, id: int) -> dict[str, any]:
+USERS = ['vlad', 'Sally', 'BOB', 'qwerty', 'Jon', '1234', 'Zeus', 'Apocriton', 'Arch']
+FIRST_ID = 1
+
+def create_user_info(user_name: str, id: int) -> dict[str, any]:
     user_ifo = {}
-    user_ifo['id'] = None
-    user_ifo['user'] = None
-    user_ifo['access_level'] = None
-    user_ifo['is_active'] = None
+    user_ifo['id'] = id
+    user_ifo['user'] = user_name
+    user_ifo['access_level'] = random.randint(1, 10)
+    user_ifo['is_active'] = random.choice([True, False])
+    db.append(user_ifo)
 
 
 db = []
-first_id = 1
-id = first_id
+id = FIRST_ID
 
 for user in USERS:
     create_user_info(user, id)
     id += 1
+
+with open(ALL_PATH, 'w', encoding=ENCODING) as f:
+    json.dump(db, f, indent=4)
 
 print(db)
